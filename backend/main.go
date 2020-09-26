1a9fc934a3b20faf02f4ada9d1cdf4598842e04d
themanofcat <31108416+themanofcat@users.noreply.github.com>
2020-09-25 21:58:20 -0400

    added two more brightness methods, one of which works

--- backend/main.go
+++ backend/main.go
@@ -38,6 +38,72 @@ func ModifyBrightness(frame *gocv.Mat, change uint8, inc bool) {
 	// no return, the changes happen to the frame's reference
 }
 
+// this method does not work, but serves as a test for getting and setting using 
+// GetUCharAt3 and SetUCharAt3; these methods don't traverse channels and therefore 
+// this isn't a useful method. I am leaving this in and commenting it for learning purposes
+func ModifyBrightness2(frame *gocv.Mat, change uint8, inc bool) {
+	if change == 0 { return }
+	
+	for i := 0; i < frame.Rows(); i++ {
+		for j := 0; j < frame.Cols(); j++ {
+			
+			for channel := 0; channel < 3; channel++ {
+				
+				// newval will eventually be the replacement value
+				var newval uint8 
+				var temp int16
+				
+				// store the result of addition/subtraction into temp
+				if inc { 
+					temp = int16(frame.GetUCharAt3(i, j, channel)) + int16(change) 
+				} else { 
+					temp = int16(frame.GetUCharAt3(i, j, channel)) - int16(change) 
+				}
+	
+				// cap the brightness of each pixel between 0 and 255
+				if temp > 255 {
+					newval = 255	
+				} else if temp < 0 {
+					newval = 0
+				} else {
+					newval = uint8(temp)
+				}
+				
+				// set the channel of the pixel to the calculated value
+				frame.SetUCharAt3(i, j, channel, newval)
+
+				// NOTE this doesn't work because you can't select channels using SetUCharAt3
+			}
+
+		}
+	}
+}
+
+// this is what I prefer the most, using the data pointer itself to quickly edit the data
+func ModifyBrightness3(frame *gocv.Mat, change int16) {
+	framedata := frame.DataPtrUint8()
+
+	// nice closure to expedite the process of keeping the values between 0 and 255
+	limit := func(val int16) uint8 { 
+		if val < 0 {
+			return 0
+		} else if val > 255 {
+			return 255
+		} else {
+			return uint8(val)
+		}
+	}
+
+	// framedata is one long array of uint8's, every third item represents a new pixel
+	// and the three in between are the BGR channels
+	for i := 0; i < len(framedata); i += 3 {
+		// done like this so we can add weights
+		framedata[i] = limit(int16(framedata[i]) + change) // B 
+		framedata[i+1] = limit(int16(framedata[i+1]) + change) // G
+		framedata[i+2] = limit(int16(framedata[i+2]) + change) // R
+	}
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("arg err") // check number of cli arguments
@@ -78,7 +144,6 @@ func main() {
 	defer curr.Close()
 	
 	for {
-		
 		// the frame curr is of type CV8UC3
 		// CV8U means it stores unsigned chars (lit. 8 bit Unsigned)
 		// C3 means it has 3 channels, these channels represent the Blue Green and Red respectively
@@ -95,7 +160,8 @@ func main() {
 		// function call;
 		// takes in a Mat*, a change value, and a boolean that asks if you're increasing or decreasing
 		// changes the frame pointed to by the Mat*
-		ModifyBrightness(&curr, 50, false) // this reduces curr's brightness by 50
+		//ModifyBrightness(&curr, 50, false) // this reduces curr's brightness by 50
+		ModifyBrightness3(&curr, -50)
 		out.Write(curr)
 
 	}
