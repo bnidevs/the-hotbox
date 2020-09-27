7f820cbb40ae3234dea9cc62dedd438c936d02a7
Bill Ni <bill.8ni@gmail.com>
2020-09-26 20:49:16 -0400

    centralize pages, upload progress bar working

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,9 @@ var bucketName = "thehotboxupload";
 var bucketRegion = 'us-east-1';
 var IdentityPoolId = 'us-east-1:2271f583-09e5-4212-b72a-4024f2cea3c5';
 
+let progbar = document.getElementById("progbar");
+let prognum = document.getElementById("progress");
+
 AWS.config.update({
   region: bucketRegion,
   credentials: new AWS.CognitoIdentityCredentials({
@@ -17,10 +20,22 @@ var s3 = new AWS.S3({
 function uploadVideo() {
   var files = document.getElementById("upload").files;
   if (!files.length) {
-    return;
+    return; // error message no file
   }
+
+  var filesize = 0;
+  for (var i = 0; i < files.length; i++){
+    filesize += files[i].size;
+  }
+
+  if (filesize > 250000000){
+    return; // error message file too big
+  }
+
   var file = files[0];
   var fileName = file.name;
+
+  document.getElementById("progress-container").style.display = "flex";
 
   var upload = new AWS.S3.ManagedUpload({
     params: {
@@ -28,11 +43,20 @@ function uploadVideo() {
       Key: fileName,
       Body: file
     }
+  }).on("httpUploadProgress", function(e){
+    progbar.value = e.loaded / filesize * 100;
+    prognum.innerText = progbar.value + "%";
   });
 
   var promise = upload.promise();
 
-  promise.then();
+  promise.then(
+    function(data) {
+      console.log("success");
+      document.getElementById("upload-label").innerText = "Upload Done";
+    },
+    function(err) {console.log("error")}
+  );
 }
 
 document.getElementById("upload-btn").addEventListener("click", uploadVideo);
