e4b90a377eed73c6c012691df29b5af44f3160e0
themanofcat <31108416+themanofcat@users.noreply.github.com>
2020-11-17 16:20:45 -0500

    read the video

--- python_backend/main.py
+++ python_backend/main.py
@@ -1,8 +1,21 @@
-from video import videoEditing
-from video import videoIO
-import cv2
+import cv2 
+import sys
+
+from video import videoEditing, videoIO
 
 
 if __name__ == '__main__':
-	print('hello')
-	print(cv2.__version__)
+    if len(sys.argv) != 2:
+        print(f"Argument error\nUsage: {sys.argv[0]} filepath")
+        sys.exit(1)
+
+    videoIn = cv2.VideoCapture(sys.argv[1])
+    
+    if videoIn.isOpened() is False:
+        print(f"Error\nUnable to read video")
+
+    
+
+
+    # CLOSE EVERYTHING
+    videoIn.release()
